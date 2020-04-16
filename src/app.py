5f8d549bac8ab72f111d6f85d5da52dacd7c8ca2
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2020-04-16 04:00:55 -0700

    Created a user session object to utilize mysql and mongodb data

--- src/app.py
+++ src/app.py
@@ -4,7 +4,8 @@ from flask import Flask, jsonify
 from flask import request
 from flask_pymongo import PyMongo
 from flask.templating import render_template
-from Models.session import Session
+from Models.Session import Session
+from Models.UserSession import UserSession
 from Services.Spark.sparkServices import produce_pi_service, split_words_service
 app= Flask(__name__)
 # connect to MongoDB with the defaults
@@ -12,18 +13,42 @@ app= Flask(__name__)
 #@app.route(..) is a decorator. A decorator is a function that takes another function and extends the behavior of the latter function without explicitly modifying it.
 #It takes a URL rule
 app.config["MONGO_URI"] = "mongodb+srv://off-top-dev:offtop123@cluster0-ci5ku.gcp.mongodb.net/off-top-db"
+app.config["JSON_SORT_KEYS"] = False
 mongo = PyMongo(app)
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-@app.route("/sessions",methods=["POST"])      
-def sessions():
+@app.route('/get-all-sessions', methods=['GET'])
+def getAllSessions():
+  session_collection = mongo.db.sessions.find({})
+  sessions = []
+  index = 0
+  for session in session_collection:
+    clean_session = {
+      'id': index,
+      "user_id":session["user_id"],
+      "first_received_at": session["first_received_at"],
+      "topic": session["topic"],
+      "focus_score": session["focus_score"],
+      "transcribed_at": session["transcribed_at"],
+      "transcribed_speech": session["transcribed_speech"]
+    }
+    sessions.append(clean_session)
+    index+=1
+    all_sessions = {
+      "sessions":sessions
+    }
+  return jsonify(all_sessions)
+  
+
+@app.route("/insert-session",methods=["POST"])      
+def insertSession():
     data = request.get_json()
     session_collection = mongo.db.sessions
     
-    new_session = Session( data["user_id"],data["first_received_at"],data["focus_score"],data["transcribed_at"],data["transcribed_speech"])
+    new_session = Session( data["user_id"],data["first_received_at"], data["topic"], data["focus_score"],data["transcribed_at"],data["transcribed_speech"])
     session_collection.insert_one(new_session.__dict__)
     output = []
     for session in session_collection.find():
@@ -32,6 +57,7 @@ def sessions():
             '_id': str(session['_id']),
             "user_id":session["user_id"],
             "first_received_at": session["first_received_at"],
+            "topic": session["topic"],
             "focus_score": session["focus_score"],
             "transcribed_at": session["transcribed_at"],
             "transcribed_speech": session["transcribed_speech"]
