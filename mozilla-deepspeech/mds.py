286f386728f4d7f2587dd63bf121173b370ef418
SMikaelian <sarkis.mikaelian.712@my.csun.edu>
2020-04-17 20:29:43 -0700

    wrapped the mozilla deepspeech command in a function to be called in another instance. Put the Stanza NLP code inside a Flask instance.

--- mozilla-deepspeech/mds.py
+++ mozilla-deepspeech/mds.py
@@ -1,2 +1,9 @@
 import os
-os.system('cmd /k "deepspeech --model deepspeech-models/output_graph.pbmm --audio audio/off-top-test.wav > output.txt"')
+from shutil import copyfile
+
+def transcribeData():
+    os.system('cmd /k "deepspeech --model deepspeech-models/output_graph.pbmm --audio audio/off-top-test.wav > output.txt"')
+    copyfile('output.txt', '/stanfordnlp//newname.ext')
+    print("Speech transcription complete.")
+
+transcribeData()
