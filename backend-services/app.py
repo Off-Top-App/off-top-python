b94c2141e0ff1e811a44b5e454f35ee02829b1fb
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2020-04-18 23:17:53 -0700

    changed the folder of src to backend-services

--- backend-services/app.py
+++ backend-services/app.py
@@ -9,7 +9,6 @@ from Models.UserSession import UserSession
 from Services.Spark.sparkServices import produce_pi_service, split_words_service
 app= Flask(__name__)
 # connect to MongoDB with the defaults
-#mongo1 = PyMongo(app, uri="mongodb://localhost:27017/off-top-db")
 #@app.route(..) is a decorator. A decorator is a function that takes another function and extends the behavior of the latter function without explicitly modifying it.
 #It takes a URL rule
 app.config["MONGO_URI"] = "mongodb+srv://off-top-dev:offtop123@cluster0-ci5ku.gcp.mongodb.net/off-top-db"
