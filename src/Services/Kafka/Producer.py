e75d7e6a67122e57f402a35fd1014849a3c5ef15
asbin4747 <asbin.dahal4747@gmail.com>
2020-04-23 12:09:23 -0700

    Updated the producer and consumer class

---
+++ src/Services/Kafka/Producer.py
@@ -0,0 +1,37 @@
+from kafka import KafkaConsumer
+from json import loads
+import json
+from datetime import datetime
+import logging
+import sys
+
+
+
+def Consumer():
+    consumer = KafkaConsumer(
+        'IncomingAudioEvent',
+        bootstrap_servers=['localhost:9092'],
+        auto_offset_reset='earliest',
+        enable_auto_commit=True,
+        group_id='my-group'
+    )
+    KafkaConsumer(value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+
+    try:
+        for incoming_message in consumer:
+            print ("%s: value=%s" % (incoming_message.topic, incoming_message.value.decode('utf-8')))
+    
+    except KeyboardInterrupt:
+        sys.exit()
+
+def main():
+    Consumer()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        format='%(asctime)s.%(msecs)s:%(name)s:' +
+               '%(levelname)s:%(process)d:%(message)s',
+        level=logging.INFO
+    )
+    main()
