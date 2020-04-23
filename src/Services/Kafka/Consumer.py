6e86e81fcf643b3813595ca5e5ad618269c33048
asbin4747 <asbin.dahal4747@gmail.com>
2020-04-23 11:14:07 -0700

    Created a producer that produces mock data

--- src/Services/Kafka/Consumer.py
+++ src/Services/Kafka/Consumer.py
@@ -2,23 +2,36 @@ from kafka import KafkaConsumer
 from json import loads
 import json
 from datetime import datetime
+import logging
+import sys
 
 
-consumer = KafkaConsumer(
-    'Food',
-     bootstrap_servers=['localhost:9092'],
-     auto_offset_reset='earliest',
-     enable_auto_commit=True,
-     group_id='my-group',
+
+def Consumer():
+    consumer = KafkaConsumer(
+        'outgoingFocusScore',
+        bootstrap_servers=['localhost:9092'],
+        auto_offset_reset='earliest',
+        enable_auto_commit=True,
+        group_id='my-group'
     )
-KafkaConsumer(value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+    KafkaConsumer(value_deserializer=lambda m: json.loads(m.decode('utf-8')))
 
-def as_payload(message):
-    return KafkaConsumer(message['user_id'])
+    try:
+        for incoming_message in consumer:
+            print ("%s: value=%s" % (incoming_message.topic, incoming_message.value.decode('utf-8')))
+    
+    except KeyboardInterrupt:
+        sys.exit()
 
-for incoming_message in consumer:
-    incoming_message = incoming_message.value
-    print("{}".format(incoming_message.decode('utf-8')) )
-    print(datetime.now())
-   
+def main():
+    Consumer()
 
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        format='%(asctime)s.%(msecs)s:%(name)s:' +
+               '%(levelname)s:%(process)d:%(message)s',
+        level=logging.INFO
+    )
+    main()
