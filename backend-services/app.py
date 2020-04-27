68fbd9a1b3ab2d4b2abd6aa51206b264212e1ccb
asbin4747 <asbin.dahal4747@gmail.com>
2020-04-26 17:05:18 -0700

    Created REST API to retrieve user info from MySQL

--- backend-services/app.py
+++ backend-services/app.py
@@ -4,14 +4,21 @@ from flask import Flask, jsonify
 from flask import request
 from flask_pymongo import PyMongo
 from flask.templating import render_template
-from Models.Session import Session
 from Models.UserSession import UserSession
+from flask_mysqldb import MySQL
+
 app= Flask(__name__)
 # connect to MongoDB with the defaults
 #@app.route(..) is a decorator. A decorator is a function that takes another function and extends the behavior of the latter function without explicitly modifying it.
 #It takes a URL rule
 app.config["MONGO_URI"] = "mongodb+srv://off-top-dev:offtop123@cluster0-ci5ku.gcp.mongodb.net/off-top-db"
 app.config["JSON_SORT_KEYS"] = False
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'offTop'
+
+mysql = MySQL(app)
 mongo = PyMongo(app)
 
 @app.route('/get-user-topic', methods=['GET'])
@@ -79,6 +86,42 @@ def insertSession():
     session_collection.insert_one(new_session)
     new_session['_id'] = str(new_session['_id'])
     return jsonify({'new_session' : new_session})
+
+  
+@app.route('/get-user-information', methods=['GET'])
+def retrieveUser():
+      cur = mysql.connection.cursor()
+      cur.execute ("SELECT * FROM user")
+      fetchdata = cur.fetchall()
+      cur.close()
+       
+      users = []
+      index = 0
+  
+      for row in fetchdata:
+          user_info = {
+              'id': index,
+              "user_id":row[0],
+              "age":row[1],
+              "city":row[2],
+              "first_name":row[6],
+              "last_name":row[8],
+              "email":row[5],
+              "gender":row[7],
+              "user_name":row[11],
+              "password":row[9],
+              "professional":row[10],
+              "created_at":row[3],
+              "deleted_at":row[4]
+            
+              }
+          users.append(user_info)
+      index+=1
+      all_users = {
+        "Users":users
+      }
+     
+      return jsonify(all_users)
 
 if __name__ == "__main__":
   app.run(debug=True)
