e18fc706332f78e08f1285a8997021deb5c96f53
SMikaelian <sarkis.mikaelian.712@my.csun.edu>
2020-04-28 02:35:29 -0700

    Reconfigured audio file processing for MDS & added data cleaning code.

--- mozilla-deepspeech/off-top-ml.py
+++ mozilla-deepspeech/off-top-ml.py
@@ -1,7 +1,7 @@
 import os
 import wave
 import deepspeech
-import deepspeech-gpu
+#import deepspeech-gpu
 import stanza
 import logging
 import datetime
@@ -11,19 +11,19 @@ from shutil import copyfile
 from timeit import default_timer as timer
 from flask import Flask, request, jsonify
 from flask_apscheduler import APScheduler
+import ffmpeg
+import librosa
 
 app= Flask(__name__)
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 global data_pipeline
+global t
 
 config = {
     'use_gpu': True,
     'processors': 'tokenize, mwt, pos, lemma, ner',
-    'lang': 'en',
-    'tokenize_model_path': './stanza_resources/en/tokenize/ewt.pt',
-    'pos_model_path': './stanza_resources/en/pos/ewt.pt',
-    'pos_pretrain_path': './stanza_resources/en/pretrain/ewt.pt',
-    'lemma_model_path': './stanza_resources/en/lemma/ewt.pt',
-    'ner_model_path': './stanza_resources/en/ner/ontonotes.pt'
+    'lang': 'en'
+
 }
 
 model_file_path= 'deepspeech-models/output_graph.pbmm'
@@ -32,6 +32,7 @@ trie_file_path = 'deepspeech-models/trie'
 BEAM_WIDTH = 500
 LM_ALPHA = 0.75
 LM_BETA = 1.85
+
 model = deepspeech.Model(model_file_path, BEAM_WIDTH)
 model.enableDecoderWithLM(lm_file_path, trie_file_path, LM_ALPHA, LM_BETA)
 sample_rate= model.sampleRate()
@@ -39,29 +40,31 @@ sample_rate= model.sampleRate()
 print("\n")
 nlp = stanza.Pipeline(**config)
 
-def transcribe(task_status):
+def transcribe():
+    t= 0
+    inference_time= 0.0
     filename= 'audio/off-top-test.wav'
     w= wave.open(filename, 'r')
-    inference_time= 0.0
     rate= w.getframerate()
     frames= w.getnframes()
     buffer= w.readframes(frames)
-    audio_length = len(audio) * (1 / sample_rate)
+    audio_length = len(buffer) * (1 / sample_rate)
 
     if (rate != sample_rate):
-        print('Warning: original sample rate (' + rate + ') is lower than ' + sample_rate + 'Hz. Up-sampling might produce erratic speech recognition.')
+        print('Warning: original sample rate ' + str(rate) + 'Hz is lower than ' + str(sample_rate) + 'Hz. Up-sampling might produce erratic speech recognition.')
 
-        data_16= np.frombuffer(buffer, dtype= np.int16)
-        type(data_16)
-
-        start= timer()
-        result= model.stt(data_16)
-        end= timer() - start
-        time+= end
-        logging.debug(task_status, str(datetime.datetime.now()))
-        logging.debug('Process took %0.3fs for %0.3fs audio file.' % (end, audio_length))
-        data_pipeline= result
-        return [result]
+    data_16= np.frombuffer(buffer, dtype= np.int16)
+    type_of= type(data_16)
+    print(type_of)
+    start= timer()
+    result= model.stt(data_16)
+    end= timer() - start
+    t+= end
+    logging.debug('Transcribed Data.', str(datetime.datetime.now()))
+    logging.debug('Process took %0.3fs for %0.3fs audio file.' % (end, audio_length))
+    data_pipeline= result
+    print('Result: ', result)
+    #return [result]
 
 def nlpProcess(task_status):
     with open("output.txt") as output:
@@ -86,7 +89,7 @@ def transcribeData():
 
 if __name__ == '__main__':
     scheduler= APScheduler()
-    scheduler.add_job(func= transcribe, args=['Transcribed Audio.'], trigger= 'interval', id= 'mds_job', seconds= 10)
-    scheduler.add_job(func= nlpProcess, args=['Analyzed Speech.'], trigger= 'interval', id= 'nlp_job', seconds= 10)
+    scheduler.add_job(func= transcribe, trigger= 'interval', id= 'mds_job', seconds= 25, max_instances= 10)
+    #scheduler.add_job(func= nlpProcess, args=['Analyzed Speech.'], trigger= 'interval', id= 'nlp_job', seconds= 25, max_instance= 10)
     scheduler.start()
     app.run(debug=True)
